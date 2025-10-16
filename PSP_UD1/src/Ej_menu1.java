7da08613cca8849006cd2d6d3f317d79303f54c2
Ana Núñez <nunez.flores.ana@iescamas.es>
2025-10-16 20:40:17 +0200

    Añado ramamenu

--- PSP_UD1/src/Ej_menu1.java
+++ PSP_UD1/src/Ej_menu1.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Ej_Menu1 {
+public class Ej_menu1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int opcion;
